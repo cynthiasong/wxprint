784b5978442528c8a90e2241c931fbcbf78aa81f
cynthiasong <cynthia.insulae@gmail.com>
2017-01-16 09:55:19 +0800

    covert css styles to inline styles

--- md2html.py
+++ md2html.py
@@ -1,25 +1,30 @@
 from mistune import Markdown
+import premailer
 
-md = Markdown()
 
 def pack_html(html):
     top = """<!DOCTYPE html><html lang="zh-cn">
           <head>
           <meta charset="UTF-8">
           <title>output</title>
-          <link rel="stylesheet" type="text/css" href="../style.css">
+          <link rel="stylesheet" type="text/css" href="style.css">
           </head>
           <body>"""
 
     bottom = """</body></html>"""
     return top + html + bottom
 
-with open('temp/test.md', 'r', encoding='utf-8') as mdfile:
-    test = mdfile.read()
+if __name__ == '__main__':
 
-with open('temp/test.html', 'w', encoding='utf-8') as htmlfile:
-    result = md(test)
-    htmlfile.write(pack_html(result))
+    with open('temp/test.md', 'r', encoding='utf-8') as mdfile:
+        mdstr = mdfile.read()
+
+    md = Markdown()
+    raw_html = md(mdstr)
+    result = premailer.transform(pack_html(raw_html))
+
+    with open('temp/test.html', 'w', encoding='utf-8') as htmlfile:
+        htmlfile.write(result)
 
 # print(pack_html(result))
 
