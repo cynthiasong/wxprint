ecddaec0c0e807ead658a26f58d9ffd644b29f49
cynthiasong <cynthia.insulae@gmail.com>
2017-01-17 11:33:37 +0800

    improve instruction

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ def pack_html(html):
     bottom = """</body></html>"""
     return top + html + bottom
 
-if __name__ == '__main__':
-
+def convert_all():
     for file in os.listdir('temp'):
 
         if file.endswith('.md'):
@@ -30,6 +29,16 @@ if __name__ == '__main__':
             with open('temp/{fn}.html'.format(fn=file[:-3]),
                       'w', encoding='utf-8') as htmlfile:
                 htmlfile.write(result)
+                print("成功：转换后的.html文件保存在temp文件夹中")
+
+if __name__ == '__main__':
+    try:
+        convert_all()
+    except:
+        input("错误：运行前请将所有要转换的.md文件放入temp文件夹中\n"
+              "按回车键结束程序：")
+
+
 
 
 
