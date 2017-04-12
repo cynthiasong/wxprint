38c9bfdaaf1e48145a48abeeb31c4cf10df0d702
cynthiasong <cynthia.insulae@gmail.com>
2017-04-12 16:55:05 +0800

    improve skeleton

--- main.py
+++ main.py
@@ -3,40 +3,43 @@ import premailer
 import os
 
 
-def pack_html(html):
-    top = """<!DOCTYPE html><html lang="zh-cn">
+def pack_html(html, style='default_style.css'):
+    head = """<!DOCTYPE html><html lang="zh-cn">
           <head>
           <meta charset="UTF-8">
           <title>output</title>
-          <link rel="stylesheet" type="text/css" href="style.css">
+          <link rel="stylesheet" type="text/css" href="{style}">
           </head>
-          <body>"""
+          <body>\n""".format(style=style)
 
-    bottom = """</body></html>"""
-    return top + html + bottom
+    bottom = """\n</body>\n</html>"""
+    return head + html + bottom
 
-def convert_all():
-    for file in os.listdir('temp'):
+
+def convert_all(src='source', dst='html_output', style='default_style.css'):
+    for file in os.listdir(src):
 
         if file.endswith('.md'):
-            with open('temp/{fn}'.format(fn=file), 'r', encoding='utf-8') as mdfile:
+            with open('{src}/{fn}'.format(src=src, fn=file), 'r', encoding='utf-8') as mdfile:
                 mdstr = mdfile.read()
 
             md = Markdown()
             raw_html = md(mdstr)
-            result = premailer.transform(pack_html(raw_html))
+            result = premailer.transform(pack_html(raw_html, style))
 
-            with open('temp/{fn}.html'.format(fn=file[:-3]),
+            with open('{dst}/{fn}.html'.format(dst=dst, fn=file[:-3]),
                       'w', encoding='utf-8') as htmlfile:
                 htmlfile.write(result)
-                print("成功：转换后的.html文件保存在temp文件夹中")
+            os.rename('{src}/{fn}'.format(src=src, fn=file), 'history/{fn}'.format(fn=file))
+            print('成功：转换后的.html文件保存在html_output文件夹中')
+            print('在history文件中查看转换完毕的.md源文件')
 
 if __name__ == '__main__':
     try:
         convert_all()
     except:
-        input("错误：运行前请将所有要转换的.md文件放入temp文件夹中\n"
-              "按回车键结束程序：")
+        input('错误：运行前请将所有要转换的.md文件放入source文件夹中'
+              '按回车键结束程序：')
 
 
 
